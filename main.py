ef6208f1a57b4804371e7b4870f8c1a9c9f43984
bela <nagy.bela.budapest@gmail.com>
2023-09-11 16:05:14 +0200

    új termék hozzáadása API-n keresztül

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 import json
+from pydantic import BaseModel
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -18,22 +19,39 @@ def load_data():
 
 termek_lista = load_data()
 
+# Pydantic modell az új termék adatainak validációjához
+class Termek(BaseModel):
+    termek_neve: str
+    leiras: str
+    ara: int  # Árat most egész számként deklaráljuk
+    kategoria: str
+
+# Főoldal
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "termek_lista": termek_lista})
 
 # Új termék hozzáadása JSON formátumban
-@app.post('/add_product')
-async def add_product(json_file: UploadFile):
+@app.post('/add_product', response_model=Termek)
+async def add_product(termek: Termek):
     try:
-        # JSON fájl beolvasása és feldolgozása
-        with json_file.file as file:
-            data = json.load(file)
+        # Az új terméknek generáljunk egyedi ID-t (például a lista hossza alapján)
+        new_id = len(termek_lista) + 1
+        termek_dict = termek.dict()
+        termek_dict["ID"] = new_id
 
-        return {"message": "Az új termék sikeresen hozzáadva."}
+        # Az új termék hozzáadása a termek_lista-hoz
+        termek_lista.append(termek_dict)
+
+        # Az új termék mentése JSON fájlba
+        with open('termek_lista.json', 'w', encoding='utf-8') as json_file:
+            json.dump(termek_lista, json_file, ensure_ascii=False, indent=4)
+
+        return termek  # Visszaadjuk az új terméket a válaszban, ami tartalmazza az ID-t
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Hibás kérés: {str(e)}")
 
+# API futtatása
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
